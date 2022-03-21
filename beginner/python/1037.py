54797596b20ddc6899dde77549a45dcfefc36978
Alexandre Pateis da Rocha <77367693+AlexandrePateis@users.noreply.github.com>
2022-03-21 13:29:46 -0300

    Update 1037.py

--- beginner/python/1037.py
+++ beginner/python/1037.py
@@ -1,12 +1,13 @@
-x = float(input());
+num = gets.to_f
 
-if x >= 0 and x <= 25.0000:
-	print("Intervalo [0,25]");
-elif x>25.0000 and x <= 50.0000:
-	print("Intervalo (25,50]");
-elif x>50.0000 and x <= 75.0000:
-	print("Intervalo (50,75]");
-elif x>75.0000 and x <= 100.0000:
-	print("Intervalo (75,100]");
-else:
-	print("Fora de intervalo");
+if num >= 0 and num <= 25.0000
+	puts"Intervalo [0,25]"
+    elsif num>25.0000 and num <= 50.0000
+	puts"Intervalo (25,50]"
+    elsif num>50.0000 and num <= 75.0000
+	puts"Intervalo (50,75]"
+    elsif num>75.0000 and num <= 100.0000
+	puts"Intervalo (75,100]"
+    else
+	puts"Fora de intervalo"
+end   
